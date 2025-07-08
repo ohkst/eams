8eab5ace7a9a270bbd82b2915cad6ef5ae8a61a4
olaf <olaf@koreainvestment.com>
2025-07-08 09:37:52 +0900

    [김성태] 문서 관리 영역 페이지 추가 및 서브미션 연동

--- WebContent/_wpack_/page/component/LeftDocTree.js
+++ WebContent/_wpack_/page/component/LeftDocTree.js
@@ -1,115 +1,15 @@
-/*amd /page/component/LeftDocTree.xml 6034 77e8f2b5a6e2252ab08c4e59a931c6915dac2596bfdc5e35a4b139ecad2509a6 */
+/*amd /page/component/LeftDocTree.xml 2644 38cf3d5ba962c338515ddc41ec60756897d8805118c7bd53071c82eb26a64fd4 */
 define({declaration:{A:{version:'1.0',encoding:'UTF-8'}},E:[{T:1,N:'html',A:{xmlns:'http://www.w3.org/1999/xhtml','xmlns:ev':'http://www.w3.org/2001/xml-events','xmlns:w2':'http://www.inswave.com/websquare','xmlns:xf':'http://www.w3.org/2002/xforms'},E:[{T:1,N:'head',A:{},E:[{T:1,N:'w2:type',E:[{T:3,text:'COMPONENT'}]},{T:1,N:'w2:buildDate'},{T:1,N:'w2:MSA'},{T:1,N:'xf:model',E:[{T:1,N:'w2:dataCollection',A:{baseNode:'map'},E:[{T:1,N:'w2:dataList',A:{baseNode:'list',repeatNode:'map',id:'dataList8',saveRemovedData:'true'},E:[{T:1,N:'w2:columnInfo',E:[{T:1,N:'w2:column',A:{id:'col1',name:'폴더',dataType:'text'}}]},{T:1,N:'w2:data',A:{use:'true'},E:[{T:1,N:'w2:row',E:[{T:1,N:'col1',E:[{T:4,cdata:'BA'}]}]},{T:1,N:'w2:row',E:[{T:1,N:'col1',E:[{T:4,cdata:'AA'}]}]},{T:1,N:'w2:row',E:[{T:1,N:'col1',E:[{T:4,cdata:'DA'}]}]},{T:1,N:'w2:row',E:[{T:1,N:'col1',E:[{T:4,cdata:'TA'}]}]}]}]}]},{T:1,N:'w2:workflowCollection'}]},{T:1,N:'w2:layoutInfo'},{T:1,N:'w2:publicInfo',A:{method:''}},{T:1,N:'script',A:{lazy:'false',type:'text/javascript'},E:[{T:4,cdata:function(scopeObj){with(scopeObj){scwin.onpageload = function () {};
-scwin.searchboxOK_onkeypress = function (e) {
-  // 엔터치면 코드 적용
-  if ((e.charCode || e.keyCode) === 13) {
-    // $p.parent().wfmRightContents.setSrc("/page/component/SystemSearchResultDetail.xml");
-    dataList1.clearFilter();
-    dataList3.clearFilter();
-    dataList4.clearFilter();
-    dataList5.clearFilter();
-    dataList7.clearFilter();
-    let jsonFilterOptions = {};
-    jsonFilterOptions.type = "row";
-    jsonFilterOptions.colIndex = "name";
-    jsonFilterOptions.key = searchboxOK.getValue();
-    jsonFilterOptions.exactMatch = false;
-    jsonFilterOptions.condition = "and";
-    dataList1.setColumnFilter(jsonFilterOptions);
-    dataList3.setColumnFilter(jsonFilterOptions);
-    dataList4.setColumnFilter(jsonFilterOptions);
-    dataList5.setColumnFilter(jsonFilterOptions);
-    dataList7.setColumnFilter(jsonFilterOptions);
-  } else {
-    // $p.log("SEARCH!!!!");
-
-    // 검색을 구현하면 될듯
-    // dataList1.clearFilter();
-
-    // let jsonFilterOptions = {};
-
-    // jsonFilterOptions.type = "row";
-    // jsonFilterOptions.colIndex = "name";
-    // jsonFilterOptions.key = searchboxOK.getValue();
-    // jsonFilterOptions.exactMatch = false;
-    // jsonFilterOptions.condition = "and";
-
-    // dataList1.setColumnFilter(jsonFilterOptions);        
-  }
+scwin.btnBA_onclick = function (e) {
+  $p.parent().wfmRightContents.setSrc("/page/component/document/BADocList.xml");
 };
-scwin.searchDetail = function (rowIndex, columnIndex, columnId, type) {
-  // $p.log(rowIndex + columnId + columnId);
-
-  var cellData = "";
-  if (type === 0) {
-    cellData = dataList1.getCellData(rowIndex, columnId);
-  } else if (type === 1) {
-    cellData = dataList3.getCellData(rowIndex, columnId);
-  } else if (type === 2) {
-    cellData = dataList4.getCellData(rowIndex, columnId);
-  } else if (type === 3) {
-    cellData = dataList5.getCellData(rowIndex, columnId);
-  } else if (type === 4) {
-    cellData = dataList7.getCellData(rowIndex, columnId);
-  }
-  $p.log(cellData);
-  var rowJSON = {
-    "Personal Info": {
-      "Name": "Lee",
-      "Age": 50,
-      "Married": false
-    },
-    "Region": "Seoul",
-    "Carrier": true,
-    "name": cellData
-  };
-
-  // 새로 열릴 화면에 데이터를 전달 (dataObject를 통해 전달 가능)
-  var obj = {
-    "dataObject": {
-      "type": "json",
-      "name": "LeftSearch",
-      "data": rowJSON
-    }
-  };
-  scwin.setContentsSrc(type, obj);
+scwin.btnAA_onclick = function (e) {
+  $p.parent().wfmRightContents.setSrc("/page/component/document/AADocList.xml");
 };
-scwin.setContentsSrc = async function (type, obj) {
-  if (type === 0) {
-    await $p.parent().wfmRightContents.setSrc("/page/component/search/SystemSearchResultBizDetail.xml", obj);
-  } else if (type === 1) {
-    await $p.parent().wfmRightContents.setSrc("/page/component/search/SystemSearchResultScreenDetail.xml", obj);
-  } else if (type === 2) {
-    await $p.parent().wfmRightContents.setSrc("/page/component/search/SystemSearchResultTRDetail.xml", obj);
-  } else if (type === 3) {
-    await $p.parent().wfmRightContents.setSrc("/page/component/search/SystemSearchResultDBDetail.xml", obj);
-  } else if (type === 4) {
-    await $p.parent().wfmRightContents.setSrc("/page/component/search/SystemSearchResultMemberDetail.xml", obj);
-  }
+scwin.btnDA_onclick = function (e) {
+  $p.parent().wfmRightContents.setSrc("/page/component/document/DADocList.xml");
 };
-
-// 업무 검색
-scwin.gridBASearchResult_oncellclick = function (rowIndex, columnIndex, columnId) {
-  scwin.searchDetail(rowIndex, columnIndex, columnId, 0);
+scwin.btnTA_onclick = function (e) {
+  $p.parent().wfmRightContents.setSrc("/page/component/document/TADocList.xml");
 };
-
-// 화면 검색
-scwin.gridAAScreenSearchResult_oncellclick = function (rowIndex, columnIndex, columnId) {
-  scwin.searchDetail(rowIndex, columnIndex, columnId, 1);
-};
-
-// TR 검색
-scwin.gridAATRSearchResult_oncellclick = function (rowIndex, columnIndex, columnId) {
-  scwin.searchDetail(rowIndex, columnIndex, columnId, 2);
-};
-
-// DB 검색
-scwin.gridDADBSearchResult_oncellclick = function (rowIndex, columnIndex, columnId) {
-  scwin.searchDetail(rowIndex, columnIndex, columnId, 3);
-};
-
-// 담당자 검색
-scwin.gridMemberSearchResult_oncellclick = function (rowIndex, columnIndex, columnId) {
-  scwin.searchDetail(rowIndex, columnIndex, columnId, 4);
-};
-}}}]}]},{T:1,N:'body',A:{'ev:onpageload':'scwin.onpageload',style:';max-width:100%;min-width:100%;'},E:[{T:1,N:'xf:group',A:{style:'width:100%;height: 100vh;background-color:#EFF0F4;padding: 12px;display: flex;flex-direction: column;overflow-y: auto;scrollbar-width: none;gap: 12px;box-sizing: border-box;',id:''},E:[{T:1,N:'w2:button',A:{'ev:onclick':'scwin.btnNetwork_onclick',id:'btnBA',label:'BA',style:'width: 100%;height: 48px;font-size:18px;font-weight:bold;text-align:left;border-width:0px;'}},{T:1,N:'w2:button',A:{'ev:onclick':'scwin.btnDB_onclick',id:'btnAA',label:'AA',style:'width: 100%;height: 48px;font-size:18px;font-weight:bold;text-align:left;border-width:0px;'}},{T:1,N:'w2:button',A:{'ev:onclick':'scwin.btnServer_onclick',id:'btnDA',label:'DA',style:'width:100%;height:48px;font-size:18px;font-weight:bold;text-align:left;border-width:0px;'}},{T:1,N:'w2:button',A:{'ev:onclick':'scwin.btnServer_onclick',id:'btnTA',label:'TA',style:'width:100%;height:48px;font-size:18px;font-weight:bold;text-align:left;border-width:0px;'}}]}]}]}]})
+}}}]}]},{T:1,N:'body',A:{'ev:onpageload':'scwin.onpageload',style:';max-width:100%;min-width:100%;'},E:[{T:1,N:'xf:group',A:{style:'width:100%;height: 100vh;background-color:#EFF0F4;padding: 12px;display: flex;flex-direction: column;overflow-y: auto;scrollbar-width: none;gap: 12px;box-sizing: border-box;',id:''},E:[{T:1,N:'w2:button',A:{id:'btnBA',label:'BA',style:'width: 100%;height: 48px;font-size:18px;font-weight:bold;text-align:left;border-width:0px;','ev:onclick':'scwin.btnBA_onclick'}},{T:1,N:'w2:button',A:{id:'btnAA',label:'AA',style:'width: 100%;height: 48px;font-size:18px;font-weight:bold;text-align:left;border-width:0px;','ev:onclick':'scwin.btnAA_onclick'}},{T:1,N:'w2:button',A:{id:'btnDA',label:'DA',style:'width:100%;height:48px;font-size:18px;font-weight:bold;text-align:left;border-width:0px;','ev:onclick':'scwin.btnDA_onclick'}},{T:1,N:'w2:button',A:{id:'btnTA',label:'TA',style:'width:100%;height:48px;font-size:18px;font-weight:bold;text-align:left;border-width:0px;','ev:onclick':'scwin.btnTA_onclick'}}]}]}]}]})
